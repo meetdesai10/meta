2e022f4d18365df662a33f7b37c6b4ed80bd3fd2
meetdesai10 <dmeet1008@gmail.com>
2024-04-10 17:14:40 +0530

    :

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage.js";
-import Header from "../src/components/Header.js";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
