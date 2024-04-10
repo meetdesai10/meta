682efdfeb4a4a5637e02ec2ff58545131d43cfac
meetdesai10 <dmeet1008@gmail.com>
2024-04-10 16:54:43 +0530

    :

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import NavBar from "../src/components/NavBar";
+import NavBar from "./components/NavBar";
 export default function App() {
   return (
     <div>
