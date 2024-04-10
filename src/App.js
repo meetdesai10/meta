49ed921a68b52608a906896a22f35ce7426b3492
meetdesai10 <dmeet1008@gmail.com>
2024-04-10 17:13:15 +0530

    :

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage.js";
-import Header from "./components/Header.js";
+import Header from "../src/components/Header.js";
 export default function App() {
   return (
     <div>
